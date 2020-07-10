520baf3a3e54906d9da70cc4aea244c67e1c18b4
arlo99 <idv19c.alopez@uartesdigitales.edu.mx>
2020-07-10 01:21:07 -0500

    solve favicon error

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const path = require('path');
 const express = require('express');
+const favicon = require('serve-favicon');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
+
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(express.static('public'));
+
 app.listen(port, () => {
    console.log('Server is up!');
 });
